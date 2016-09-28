f335adfa9dec68ea7ed81dbe61c5bd384086f3ff
qinyj <cdyjqin@cn.ibm.com>
2016-09-28 14:09:40 +0800

    update db info

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@
  * Created by qinyj on 6/24/2016.
  */
 var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/rctsdb');
+mongoose.connect('mongodb://rctsusr:rctspwd@169.50.117.136:27017/rctsdb');
 var db=mongoose.connection;
 //get notify if connect successfully or if a connection error occurs
 db.on('error',console.error.bind(console,'connection error:'));
